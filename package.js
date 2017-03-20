15c61b3aaee2136438c53b440fe191a09d79de8f
Artur Nerkowski <artur@reaktor5.com>
2017-03-20 04:58:40 +0100

    Added tests

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'apatryda:pug-compiler',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Meteor plugin for importing Pug templates',
   git: 'https://github.com/apatryda/meteor-pug-compiler.git',
   documentation: 'README.md'
@@ -23,4 +23,16 @@ Package.registerBuildPlugin({
 Package.onUse(function(api) {
   api.versionsFrom('1.3');
   api.use('isobuild:compiler-plugin@1.0.0');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('apatryda:pug-compiler');
+  api.addFiles([
+    'tests/test1.pug',
+    'tests/test2.pug',
+    'tests/test3.pug',
+  ], 'server');
+  api.mainModule('pug-compiler-tests.js');
 });
---
+++ pug-compiler-tests.js
@@ -0,0 +1,24 @@
+// Import Tinytest from the tinytest Meteor package.
+import { Tinytest } from "meteor/tinytest";
+
+import test1 from './tests/test1.pug';
+import test2 from './tests/test2.pug';
+import test3 from './tests/test3.pug';
+
+Tinytest.add('pug-compiler - test1', function (test) {
+  test.equal(test1,
+    '<div class="message" id="test1">Hello world!</div>'
+  );
+});
+
+Tinytest.add('pug-compiler - test2', function (test) {
+  test.equal(test2,
+    '<div id="test2"><ul><li class="one"><a href="#">Link 1</a></li><li class="two"><a href="#">Link 2</a></li><li class="three"><a href="#">Link 3</a></li></ul></div>'
+  );
+});
+
+Tinytest.add('pug-compiler - test3', function (test) {
+  test.equal(test3,
+    '<div id="test3"><article><header><h2>Some title</h2></header><section><p>A first paragraph</p><p>A second multiline paragraph</p></section></article></div>'
+  );
+});
