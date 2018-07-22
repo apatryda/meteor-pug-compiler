7a6d95a6faebbb0d5ce4aba46063aa9cd1294733
Artur Nerkowski <artur@reaktor5.com>
2018-07-22 23:58:10 +0200

    Bumped package versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'apatryda:pug-compiler',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Meteor plugin for importing Pug templates',
   git: 'https://github.com/apatryda/meteor-pug-compiler.git',
   documentation: 'README.md'
@@ -9,19 +9,19 @@ Package.describe({
 Package.registerBuildPlugin({
   name: 'pug-compiler',
   use: [
-    'urigo:static-html-compiler@0.1.8',
-    'ecmascript@0.2.0',
+    'urigo:static-html-compiler@1.0.0',
+    'ecmascript@0.11.1',
   ],
   sources: [
     'pug-compiler.js',
   ],
   npmDependencies: {
-    'pug': '2.0.0-rc.3',
+    'pug': '2.0.3',
   },
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
+  api.versionsFrom('METEOR@1.6.1');
   api.use('isobuild:compiler-plugin@1.0.0');
 });
 
